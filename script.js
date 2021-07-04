d873a9eb44f13fc395241382c7c5dde26250dffc
bigyan_lamsal <bigyanlamsal@gmail.com>
2021-07-04 20:05:13 +0545

    commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Web Developer", "Mobile App Developer", "Designer", "Freelancer"],
+        strings: [ "Web Developer", "Mobile App Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Web Developer", "Mobile App Developer", "Designer", "Freelancer"],
+        strings: [ "Web Developer", "Mobile App Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
